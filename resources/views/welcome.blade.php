16b9c998129c01c4803ff3d0f1412dc81f4f82e8
ShihabMahmod <58717265+ShihabMahmod@users.noreply.github.com>
2023-02-16 09:07:27 +0600

    Socialite

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+
+    <title>Login With Google</title>
+    </head>
+    <body class="antialiased">
+            <div>
+                <a href="{{route('loginwithgoogle')}}"><h2>Log in with Google</h2></a>
+            </div>
+    </body>
+</html>
